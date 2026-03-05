11aa6ffcb6dc471157b80d3ff41d0eb72ae2a81d
Amy <0814233040@s.tusd1.org>
2026-03-05 21:06:13 +0000

    Almost!

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ console.log("Script started");
 // Variable to track the number of clicks.
 let clicks = 0;
 
+// Click value
+let clickValue = 1;
+
 // Variable to store the click display element. 
 let clickDisplay = document.getElementById("click-display");
+
+//VAriable for level display.
+let levelDisplay = document.getElementById("level-display");
 
 // Set the initial of click display.
 clickDisplay.innerText = "Clicks: " + clicks;
@@ -14,8 +20,15 @@ function handleClick() {
     console.log("click");
 
     // Count the click.
-    clicks = clicks + 1;
+    clicks = clicks + clickValue;
 
     // Update display with the new value.
     clickDisplay.innerText = "Clicks: " + clicks;
+
+    if (clicks == 10) {
+        alert("Congrats on making it to Level two!")
+        levelDisplay.innerText = "Level 2";
+        clickValue = 2;
+    }
+    
 }
